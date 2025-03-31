daa369a9ed37b5ff4fb93c7e2b98c88dc20d5fa6
duyenpham95 <duyenpham.june.1995@gmail.com>
2025-03-31 13:52:21 +1100

    Create rotate with left and right arrow key

--- Assets/Script/Moving.cs
+++ Assets/Script/Moving.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class Moving : MonoBehaviour
 {
     float translationSpeed = 2.0f;
+    float rotationSpeed = 100.0f;
 
     void Update()
     {
         float translation = Input.GetAxis("Vertical") * translationSpeed * Time.deltaTime;
         transform.Translate(0, 0, translation);
+
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
     }
 }
